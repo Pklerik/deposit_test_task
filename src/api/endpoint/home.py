370250abbfd024c9ddb559df44bdafaab3973fe7
Pavel Budkov <pklerik@mail.ru>
2024-02-09 16:37:46 +0300

    [new] - deleted home route

--- src/api/endpoint/home.py
+++
@@ -1,9 +0,0 @@
-from fastapi import APIRouter
-from src.deposit.schemas import Deposit
-from src.deposit import service
-
-router = APIRouter()
-
-@router.get("/")
-async def home():
-    return "message"
--- src/api/routers.py
+++ src/api/routers.py
@@ -4,5 +4,4 @@ from src.api.endpoint import deposit, home
 
 api_router = APIRouter()
 api_router.include_router(deposit.router, tags=["deposit"])
-api_router.include_router(home.router, tags=["home"])
 
