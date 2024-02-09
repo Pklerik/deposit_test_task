5f095ba658664e76ae511715c93fb572755a05c2
Pavel Budkov <pklerik@mail.ru>
2024-02-09 17:29:50 +0300

    [new] - add response with status code 200

--- src/api/endpoint/deposit.py
+++ src/api/endpoint/deposit.py
@@ -1,4 +1,6 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, status, HTTPException
+from fastapi.responses import JSONResponse
+from fastapi.responses import JSONResponse
 from src.deposit.schemas import Deposit
 from src.deposit import service
 
@@ -6,4 +8,10 @@ router = APIRouter()
 
 @router.post("/deposit")
 async def deposit_calculation(deposit_data:Deposit) -> dict:
-    return service.calculate_deposit(deposit_data)
+    return JSONResponse(status_code=status.HTTP_200_OK, content=service.calculate_deposit(deposit_data))
+
+
+
+# @router.exception_handler(RequestValidationError)
+# async def validation_exception_handler(request, exc):
+#     return PlainTextResponse(str(exc), status_code=404)
