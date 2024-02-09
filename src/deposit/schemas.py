6845315f698cd1cfc881b65b2c6d74068e45518c
Pavel Budkov <pklerik@mail.ru>
2024-02-09 17:28:50 +0300

    [fix] - rename date based on task

--- src/deposit/schemas.py
+++ src/deposit/schemas.py
@@ -1,17 +1,17 @@
 
-from datetime import datetime, date
+from datetime import datetime, date as date_cl
 from pydantic import BaseModel, Field, validator
 
 
 class Deposit(BaseModel):
-    date_of_request:str #TODO написать валидацию для даты?
+    date:str
     periods:int = Field(..., ge=1, le=60)
     amount:int = Field(..., ge=10_000, le=3_000_000)
     rate:float = Field(..., ge=1.00, le=8.00)
 
-    @validator('date_of_request')
+    @validator('date')
     @classmethod
-    def validate_str_date(cls, date_input:date) -> date: #
+    def validate_str_date(cls, date_input:date_cl) -> date_cl: #
         """_summary_
 
         Args:
--- src/deposit/service.py
+++ src/deposit/service.py
@@ -14,7 +14,7 @@ def calculate_deposit(deposit_data:schemas.Deposit) -> dict:
     profit_by_dates = {}
     amount = deposit_data.amount
     for iteration in range(1, deposit_data.periods + 1):
-        date_of_payment = str(deposit_data.date_of_request + relativedelta(months=iteration - 1))
+        date_of_payment = str(deposit_data.date + relativedelta(months=iteration - 1))
         amount = ((12 * 100) + deposit_data.rate) * amount / (12 * 100)
         profit_by_dates.update({ date_of_payment : format(round(amount, 2), '.2f')})
     return profit_by_dates
