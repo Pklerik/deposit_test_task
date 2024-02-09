2b752ad1d0624b7837696e1f56e19fea0cc035ef
Pavel Budkov <pklerik@mail.ru>
2024-02-09 16:38:33 +0300

    [mnt] - renamed date variable

--- src/deposit/schemas.py
+++ src/deposit/schemas.py
@@ -1,15 +1,25 @@
-from datetime import datetime
+# pylint: disable=E0213
+
+from datetime import datetime, date
 from typing import Dict
 from pydantic import BaseModel, Field, validator
 
 
 
 class Deposit(BaseModel):
-    date:str #TODO написать валидацию для даты?
+    date_of_request:str #TODO написать валидацию для даты?
     periods:int = Field(..., ge=1, le=60)
     amount:int = Field(..., ge=10_000, le=3_000_000)
     rate:float = Field(..., ge=1.00, le=8.00)
 
-    @validator('date')
-    def validate_str_date(cls, date_input):
+    @validator('date_of_request')
+    def validate_str_date(cls, date_input:date) -> date: #
+        """_summary_
+
+        Args:
+            date_input (date): date of request
+
+        Returns:
+            date: date of request after validation on format <%d.%m.%Y>
+        """
         return datetime.strptime(date_input, '%d.%m.%Y').date()  # convert string to date
