96722b3664da6ba19fb8de9b75ad9860faca790a
Pavel Budkov <pklerik@mail.ru>
2024-02-09 19:30:27 +0300

    [new] - added custom error handler

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, status
+from fastapi.responses import JSONResponse
+from fastapi.encoders import jsonable_encoder
+from fastapi.exceptions import RequestValidationError
 from src.api import routers
 
 deposit_app = FastAPI(
@@ -7,3 +10,10 @@ deposit_app = FastAPI(
     version="0.1",)
 
 deposit_app.include_router(routers.api_router)
+
+@deposit_app.exception_handler(RequestValidationError)
+async def validation_exception_handler(request, exc):
+    return JSONResponse(
+        status_code=status.HTTP_400_BAD_REQUEST,
+        content=jsonable_encoder({"error": exc.errors()}),
+    )
