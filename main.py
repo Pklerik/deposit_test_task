03c8301449f8145d2e30ef2806a1c4b247fa26d0
Pavel Budkov <pklerik@mail.ru>
2024-02-10 11:02:50 +0300

    [mnt] - added if name main

--- main.py
+++ main.py
@@ -5,15 +5,16 @@ from fastapi.exceptions import RequestValidationError
 from src.api import routers
 
 deposit_app = FastAPI(
-    title="Deposit calculation FastAPI",
-    description="Author - PABUD4",
-    version="0.1",)
+        title="Deposit calculation FastAPI",
+        description="Author - PABUD4",
+        version="0.1",)
 
-deposit_app.include_router(routers.api_router)
+if __name__ == "__main__":
+    deposit_app.include_router(routers.api_router)
 
-@deposit_app.exception_handler(RequestValidationError)
-async def validation_exception_handler(request, exc):
-    return JSONResponse(
-        status_code=status.HTTP_400_BAD_REQUEST,
-        content=jsonable_encoder({"error": exc.errors()}),
-    )
+    @deposit_app.exception_handler(RequestValidationError)
+    async def validation_exception_handler(request, exc):
+        return JSONResponse(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            content=jsonable_encoder({"error": exc.errors()}),
+        )
