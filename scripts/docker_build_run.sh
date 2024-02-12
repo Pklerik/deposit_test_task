3f9ebcc5c4663e14cc6d646edfe4ed86b888d4b8
Pavel Budkov <pklerik@mail.ru>
2024-02-12 22:22:34 +0300

    [mnt] - added test info log

--- scripts/docker_build_run.sh
+++ scripts/docker_build_run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "INFO : Check if docker running"
-if ! docker info > /dev/null 2>&1; 
+if ! docker info > /dev/null 2>&1;
     then
         echo "INFO : This script uses docker, and it isn't running - please start docker and try again!"
         exit 1
@@ -13,6 +13,7 @@ echo "INFO : Bulding docker image"
 docker build --tag deposit_service:latest .
 echo "INFO : Buid complited"
 
+echo "INFO : Run tests"
 docker run --rm -t --entrypoint="pytest" deposit_service:latest
 
 if [ $? -eq 0 ];
